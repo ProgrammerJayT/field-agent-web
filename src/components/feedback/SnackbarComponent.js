500a3c6f2971331b1de6d1dfd621669dd3e75b86
ProgrammerJayT <theanthem8@gmail.com>
2024-02-05 13:29:21 +0200

    Refactored snackbar

--- src/components/feedback/SnackbarComponent.js
+++ src/components/feedback/SnackbarComponent.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
-function FormSnackbar({ open, onClose, message }) {
+const SnackbarComponent = ({ open, message, type }) => {
   return (
-    <Snackbar
-      open={open}
-      autoHideDuration={6000}
-      onClose={onClose}
-      message={message}
-    />
+    <div>
+      <Snackbar open={open}>
+        <Alert severity={type} variant="filled" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
   );
-}
+};
 
-export default FormSnackbar;
+export default SnackbarComponent;
