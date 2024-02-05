eba49075108aa3aac54013478da36245215cb497
ProgrammerJayT <theanthem8@gmail.com>
2024-02-06 01:55:48 +0200

    Added token setter to local storage

--- src/services/server/auth/login.js
+++ src/services/server/auth/login.js
@@ -3,12 +3,16 @@ import axios from "axios";
 import { headers } from "../defaults/axiosDefaults";
 
 export const login = async (credentials) => {
-
   try {
     const res = await axios.post(`${BASE_URL}/api/v1/login`, {
       email: credentials.email,
       password: credentials.password,
     });
+
+    if (res?.status === 200) {
+      headers();
+      localStorage.setItem("token", res.data.token);
+    }
 
     console.log("Login res", res);
 
