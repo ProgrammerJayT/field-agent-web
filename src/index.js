9d91ff692c97dfc5a0411f8bc8ebe5e441792c9f
ProgrammerJayT <theanthem8@gmail.com>
2024-02-06 01:54:06 +0200

    Imported axios headers setter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { headers } from "./services/server/defaults/axiosDefaults";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -30,3 +31,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+headers()
