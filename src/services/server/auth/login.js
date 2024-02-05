729d52f693f7e3d35febe523ef862e0f5691a6d3
ProgrammerJayT <theanthem8@gmail.com>
2024-02-05 13:29:38 +0200

    Refactored login request

--- src/services/server/auth/login.js
+++ src/services/server/auth/login.js
@@ -3,12 +3,17 @@ import axios from "axios";
 import { headers } from "../defaults/axiosDefaults";
 
 export const login = async (credentials) => {
-  const res = await axios.post(`${BASE_URL}/api/v1/login`, {
-    email: credentials.email,
-    password: credentials.password,
-  });
 
-  console.log("Login res", res);
+  try {
+    const res = await axios.post(`${BASE_URL}/api/v1/login`, {
+      email: credentials.email,
+      password: credentials.password,
+    });
 
-  return res.data;
+    console.log("Login res", res);
+
+    return res;
+  } catch (error) {
+    return error;
+  }
 };
