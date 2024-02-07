0f5f2e8a1278d4c921f0b1e0046c9efcb939abe3
ProgrammerJayT <theanthem8@gmail.com>
2024-02-07 23:45:04 +0200

    Implemented user check

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getLoggedInUser } from "../services/server/auth/getLoggedInUser";
+import FullscreenLoader from "../components/FullscreenLoader";
+import { checkUser } from "../utils/middleware/checkUser";
+import { useNavigate } from "react-router-dom";
+import CustomerList from "./customer/CustomerList";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const navigate = useNavigate();
+  const [authChecked, setAuthChecked] = useState(false);
+
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getLoggedInUser(),
+    enabled: authChecked, // Only fetch when authChecked is true
+  });
+
+  useEffect(() => {
+    const onCheckUser = async () => {
+      const isUser = await checkUser();
+      console.log("Is user", isUser);
+
+      if (!isUser) {
+        setTimeout(() => {
+          return navigate("/auth/login");
+        }, 5000);
+      }
+
+      setAuthChecked(true);
+    };
+
+    onCheckUser();
+  }, [navigate]);
+
+  if (userQuery.isError) {
+    // Error occurred while fetching user data
+    return <div>Error fetching user data</div>;
+  }
+
+  // User data successfully fetched, you can now access userQuery.data
+  // console.log("User response", userQuery.data);
+
+  return (
+    <div style={{}}>
+      {(!authChecked || userQuery.isLoading) && <FullscreenLoader />}
+
+      <CustomerList />
+    </div>
+  );
+};
 
 export default Home;
