2d3833b0d006507d71e79a7644015bf24c51395c
ProgrammerJayT <theanthem8@gmail.com>
2024-02-07 23:03:48 +0200

    Added backdrop

--- src/components/FullscreenLoader.js
+++ src/components/FullscreenLoader.js
@@ -1,36 +1,41 @@
 import React from "react";
 import ReactLoading from "react-loading";
 import { Typography } from "@mui/material";
-import { blue, red } from "@mui/material/colors";
+import Backdrop from "@mui/material/Backdrop";
 
 function FullscreenLoader({ message }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
+    <Backdrop
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={true}
+      // onClick={handleClose}
     >
-      <ReactLoading
-        type="spinningBubbles"
-        color="primary"
-        height={100}
-        width={50}
-      />
-
-      <Typography
-        variant="h6"
-        sx={{
-          textAlign: "center",
-          color: blue,
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        {message || "Zuse Technologies"}
-      </Typography>
+        <ReactLoading
+          type="spinningBubbles"
+          color="white"
+          height={100}
+          width={50}
+        />
 
-      <Typography variant="p">Please wait...</Typography>
-    </div>
+        <Typography
+          variant="h6"
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          {message || "Zuse Technologies"}
+        </Typography>
+
+        <Typography variant="p">Please wait...</Typography>
+      </div>
+    </Backdrop>
   );
 }
 
