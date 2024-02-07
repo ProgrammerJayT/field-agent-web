f655e641d364117f55d6a3411eed07e09469e1d6
ProgrammerJayT <theanthem8@gmail.com>
2024-02-07 23:03:23 +0200

    Added app bar component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import RouteStack from "./router/RouteStack";
+import AppBarComponent from "./components/layout/AppBarComponent";
 
 const defaultTheme = createTheme();
 
@@ -13,19 +14,18 @@ const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Container
-        component="main"
-        maxWidth="xs"
-        sx={{
-          display: "flex",
+
+      <main
+        style={{
+          width: "100vw",
+          height: "100vh",
           flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "100vh",
+          display: "flex",
         }}
       >
+        <AppBarComponent />
         <RouteStack />
-      </Container>
+      </main>
     </ThemeProvider>
   );
 };
