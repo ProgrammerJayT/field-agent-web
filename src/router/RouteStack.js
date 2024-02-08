872d5de64f27cc4876efb96b353a12651251ac28
ProgrammerJayT <theanthem8@gmail.com>
2024-02-09 01:47:59 +0200

    Added margin top

--- src/router/RouteStack.js
+++ src/router/RouteStack.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../views/auth/Login";
 import Home from "../views/Home";
 import { Container } from "@mui/material";
+import CustomerCreate from "../views/customer/CustomerCreate";
 
 const RouteStack = () => {
   return (
-    <Container>
+    <Container sx={{marginTop:4}}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
+          <Route path="/customers/create" element={<CustomerCreate />} />
         </Routes>
       </BrowserRouter>
     </Container>
