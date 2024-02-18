7e1b37a02609169583dd393ebb09de92f810c9fc
ProgrammerJayT <theanthem8@gmail.com>
2024-02-18 12:05:34 +0200

    Created customer create request

---
+++ src/services/server/customers/createCustomer.js
@@ -0,0 +1,19 @@
+import { BASE_URL } from "../defaults/baseUrl";
+import axios from "axios";
+
+export const createCustomer = async (customer) => {
+  try {
+    const res = await axios.post(`${BASE_URL}/api/v1/users`, {
+      name: customer.name,
+      surname: customer.surname,
+      email: customer.email,
+      latitude: customer.latitude,
+      longitude: customer.longitude,
+      role: customer.role,
+    });
+
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
