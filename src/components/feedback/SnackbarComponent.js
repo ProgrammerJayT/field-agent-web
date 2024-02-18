73a8db6aad9e4efae3095ef54676f00a2dfaccdf
ProgrammerJayT <theanthem8@gmail.com>
2024-02-18 12:02:54 +0200

    Added props listener

--- src/components/feedback/SnackbarComponent.js
+++ src/components/feedback/SnackbarComponent.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 
-const SnackbarComponent = ({ open, message, type }) => {
+export default function SnackbarComponent({ open, message, type }) {
+  const [snackbar, setSnackbar] = useState({
+    open: false,
+    message: "",
+    type: "",
+  });
+
+  useEffect(() => {
+    setSnackbar((prev) => ({
+      ...prev,
+      open: open,
+    }));
+  }, [open, message, type]);
+
   return (
     <div>
-      <Snackbar open={open}>
+      <Snackbar open={snackbar.open}>
         <Alert severity={type} variant="filled" sx={{ width: "100%" }}>
           {message}
         </Alert>
       </Snackbar>
     </div>
   );
-};
-
-export default SnackbarComponent;
+}
