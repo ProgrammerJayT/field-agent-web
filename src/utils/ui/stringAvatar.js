5a4cdef9f05ecb5dac0671316c6481f912f02117
ProgrammerJayT <theanthem8@gmail.com>
2024-02-18 12:07:54 +0200

    Created avatar processor

---
+++ src/utils/ui/stringAvatar.js
@@ -0,0 +1,10 @@
+import { stringToColor } from "./stringToColour";
+
+export const stringAvatar = (name) => {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+};
