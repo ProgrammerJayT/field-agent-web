3e089ac88c066d52e37c5f07c523a81020eef723
ProgrammerJayT <theanthem8@gmail.com>
2024-02-18 12:00:02 +0200

    Wrapped entry file with browser router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Login from "./views/auth/Login";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import RouteStack from "./router/RouteStack";
 import AppBarComponent from "./components/layout/AppBarComponent";
+import { BrowserRouter } from "react-router-dom";
 
 const defaultTheme = createTheme();
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-
-      <main
-        style={{
-          width: "100vw",
-          height: "100vh",
-          flexDirection: "column",
-          display: "flex",
-        }}
-      >
+      <main>
+        <CssBaseline />
         <AppBarComponent />
-        <RouteStack />
+
+        <BrowserRouter>
+          <RouteStack />
+        </BrowserRouter>
       </main>
     </ThemeProvider>
   );
