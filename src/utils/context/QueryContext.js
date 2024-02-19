d8273b303d8372fae5b6716c5ea72da4144672c0
ProgrammerJayT <theanthem8@gmail.com>
2024-02-19 02:02:08 +0200

    Added queries and invalidation methods

--- src/utils/context/QueryContext.js
+++ src/utils/context/QueryContext.js
@@ -1,20 +1,55 @@
 // QueryContext.js
-import { useQuery } from "@tanstack/react-query";
-import React, { createContext, useContext, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { getCustomers } from "../../services/server/customers/getCustomers";
+import { getLoggedInUser } from "../../services/server/auth/getLoggedInUser";
+import { getCustomersViews } from "../../services/server/customers/getCustomersViews";
+import { getCustomer } from "../../services/server/customers/getCustomer";
 
 const QueryContext = createContext();
 
 export const useQueryContext = () => useContext(QueryContext);
 
 export const QueryProvider = ({ children }) => {
+  const [authChecked, setAuthChecked] = useState(false);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    console.log("Auth was checked", authChecked);
+  }, [authChecked]);
+
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: getLoggedInUser,
+    enabled: authChecked,
+  });
+
   const customersQuery = useQuery({
     queryKey: ["customers"],
     queryFn: getCustomers,
+    enabled: authChecked && userQuery.data != null,
+  });
+
+  const useCustomerQuery = (id) => {
+    return useQuery({
+      queryKey: ["customer", id],
+      queryFn: () => getCustomer(id),
+    });
+  };
+
+  const customersViewsQuery = useQuery({
+    queryKey: ["customersViews"],
+    queryFn: getCustomersViews,
+    enabled: authChecked && userQuery.data != null,
   });
 
   const invalidateCustomersQuery = () => {
-    //
+    queryClient.invalidateQueries(["customers"], { exact: true });
+  };
+
+  const invalidateCustomersViewsQuery = (views) => {
+    queryClient.invalidateQueries(["customersViews"], { exact: true });
+    queryClient.setQueryData(["customersViews"], views);
   };
 
   const invalidateOrdersQuery = () => {
@@ -29,12 +64,18 @@ export const QueryProvider = ({ children }) => {
     <QueryContext.Provider
       value={{
         //variables
+        userQuery,
+        authChecked,
         customersQuery,
+        customersViewsQuery,
 
         //methods
-        invalidateCustomersQuery,
+        setAuthChecked,
+        useCustomerQuery,
         invalidateOrdersQuery,
         invalidateProductsQuery,
+        invalidateCustomersQuery,
+        invalidateCustomersViewsQuery,
       }}
     >
       {children}
